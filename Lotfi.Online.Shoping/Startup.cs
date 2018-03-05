ec781afd8448e8fb322c1a5c3fa8aef5d055f8b1
Mhammad lotfi <lotfi.engin@gmail.com>
2018-03-05 13:52:59 +0330

    s

--- Lotfi.Online.Shoping/Startup.cs
+++ Lotfi.Online.Shoping/Startup.cs
@@ -12,7 +12,7 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace Lotfi.OnlineShoping
 {
-    //this is for test commit
+    //this is for test commit all
     public class Startup
     {
         // This method gets called by the runtime. Use this method to add services to the container.
